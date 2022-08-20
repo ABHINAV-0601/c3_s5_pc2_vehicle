9ca6c9750ae3ab7c9ff06d348ff0b95b4298531d
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-20 16:36:28 +0530

    created a parameterized constructor and getter setter methods

--- src/com/vehicles/VehicleManufacturer.java
+++ src/com/vehicles/VehicleManufacturer.java
@@ -4,4 +4,28 @@ public abstract class VehicleManufacturer {
     private String vehicleName;
     private String vehicleModelNmae;
     private String vehicleType;
+
+    public VehicleManufacturer(String vehicleName, String vehicleModelNmae, String vehicleType) {
+        this.vehicleName = vehicleName;
+        this.vehicleModelNmae = vehicleModelNmae;
+        this.vehicleType = vehicleType;
+    }
+    public String getVehicleName() {
+        return vehicleName;
+    }
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
+    public String getVehicleModelNmae() {
+        return vehicleModelNmae;
+    }
+    public void setVehicleModelNmae(String vehicleModelNmae) {
+        this.vehicleModelNmae = vehicleModelNmae;
+    }
+    public String getVehicleType() {
+        return vehicleType;
+    }
+    public void setVehicleType(String vehicleType) {
+        this.vehicleType = vehicleType;
+    }
 }
